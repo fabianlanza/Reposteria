10385eb2369e873f9758bf121783deaefcbbe822
Z7RO <f7.z7ro@gmail.com>
2023-11-26 22:26:21 -0600

    elimine button

--- Pasteleria/Form1.Designer.vb
+++ Pasteleria/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Inicio
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,75 +20,64 @@ Partial Class Inicio
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.btnCompra = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.btnInventario = New System.Windows.Forms.Button()
         Me.btnHistorial = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnCompra
         '
-        Me.btnCompra.Location = New System.Drawing.Point(45, 128)
-        Me.btnCompra.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnCompra.Location = New System.Drawing.Point(60, 158)
+        Me.btnCompra.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.btnCompra.Name = "btnCompra"
-        Me.btnCompra.Size = New System.Drawing.Size(100, 24)
+        Me.btnCompra.Size = New System.Drawing.Size(133, 30)
         Me.btnCompra.TabIndex = 7
         Me.btnCompra.Text = "Realizar compra"
         Me.btnCompra.UseVisualStyleBackColor = True
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(371, 128)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Button1.Location = New System.Drawing.Point(495, 158)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(118, 24)
+        Me.Button1.Size = New System.Drawing.Size(157, 30)
         Me.Button1.TabIndex = 8
         Me.Button1.Text = "Acceso de empleado"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'btnInventario
         '
-        Me.btnInventario.Location = New System.Drawing.Point(371, 205)
-        Me.btnInventario.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnInventario.Location = New System.Drawing.Point(495, 252)
+        Me.btnInventario.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.btnInventario.Name = "btnInventario"
-        Me.btnInventario.Size = New System.Drawing.Size(118, 24)
+        Me.btnInventario.Size = New System.Drawing.Size(157, 30)
         Me.btnInventario.TabIndex = 9
         Me.btnInventario.Text = "Inventario"
         Me.btnInventario.UseVisualStyleBackColor = True
         '
         'btnHistorial
         '
-        Me.btnHistorial.Location = New System.Drawing.Point(371, 285)
-        Me.btnHistorial.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnHistorial.Location = New System.Drawing.Point(495, 351)
+        Me.btnHistorial.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.btnHistorial.Name = "btnHistorial"
-        Me.btnHistorial.Size = New System.Drawing.Size(118, 24)
+        Me.btnHistorial.Size = New System.Drawing.Size(157, 30)
         Me.btnHistorial.TabIndex = 10
         Me.btnHistorial.Text = "Historial"
         Me.btnHistorial.UseVisualStyleBackColor = True
         '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(126, 286)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 11
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
         'Inicio
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(532, 388)
-        Me.Controls.Add(Me.Button2)
+        Me.ClientSize = New System.Drawing.Size(709, 478)
         Me.Controls.Add(Me.btnHistorial)
         Me.Controls.Add(Me.btnInventario)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.btnCompra)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Inicio"
         Me.Text = "Inicio"
         Me.ResumeLayout(False)
@@ -98,5 +87,4 @@ Partial Class Inicio
     Friend WithEvents Button1 As Button
     Friend WithEvents btnInventario As Button
     Friend WithEvents btnHistorial As Button
-    Friend WithEvents Button2 As Button
 End Class
