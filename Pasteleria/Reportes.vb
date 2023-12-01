73f2d9baaafd39e5bc2620ccc44353453c986e89
Fabian1 <60055343+fabianlanza@users.noreply.github.com>
2023-11-30 23:01:09 -0600

    Reporte

--- Pasteleria/Reportes.vb
+++ Pasteleria/Reportes.vb
@@ -2,6 +2,8 @@
 Imports iText.Kernel.Pdf
 Imports iText.Layout.Element
 Imports System.IO
+Imports System.Data.SqlClient
+
 
 Public Class Reportes
     Private Sub Reportes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -12,18 +14,26 @@ Public Class Reportes
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
-            Dim tempFolderPath As String = Path.GetTempPath()
+            ' Query SQL para seleccionar datos de la tabla "cliente"
+            Dim query As String = "SELECT * FROM Cliente"
 
-            ' Combinar la ruta temporal con el nombre del archivo PDF
-            Dim filePath As String = Path.Combine(tempFolderPath, "output.pdf")
+            ' Obtener los datos de la base de datos utilizando la función LoadData de tu módulo 'Conexion'
+            Dim data As String = GetDataFromDatabase(query)
 
-            ' Crear un nuevo documento PDF en la carpeta temporal
+            ' Obtener la ruta de la carpeta Descargas
+            Dim downloadsPath As String = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+            downloadsPath = Path.Combine(downloadsPath, "Downloads")
+
+            ' Ruta del archivo PDF
+            Dim filePath As String = Path.Combine(downloadsPath, "clientes.pdf")
+
+            ' Crear un nuevo documento PDF en la carpeta Descargas
             Using pdfDoc As New PdfDocument(New PdfWriter(filePath))
                 ' Crear un documento
                 Dim document As New Document(pdfDoc)
 
-                ' Agregar contenido al PDF
-                document.Add(New Paragraph("Hello World!"))
+                ' Agregar los datos obtenidos de la base de datos al PDF como un párrafo
+                document.Add(New Paragraph(data))
 
                 ' Cerrar el documento
                 document.Close()
@@ -33,7 +43,7 @@ Public Class Reportes
             Process.Start(filePath)
 
         Catch ex As Exception
-            MessageBox.Show("Error al generar el PDF: " & ex.Message)
+            MessageBox.Show("Error: " & ex.Message)
         End Try
     End Sub
 
@@ -41,7 +51,33 @@ Public Class Reportes
 
 
 
+    Private Function GetDataFromDatabase(query As String) As String
+        Dim data As String = ""
 
+        Try
+            ' Crear la conexión a la base de datos utilizando tu conexión existente
+            Using connection As New SqlConnection(Conexion.connect.ConnectionString)
+                ' Abrir la conexión
+                connection.Open()
+
+                ' Crear el comando SQL
+                Using command As New SqlCommand(query, connection)
+                    ' Ejecutar el comando y obtener los datos
+                    Using reader As SqlDataReader = command.ExecuteReader()
+                        ' Leer los datos y almacenarlos en la cadena 'data'
+                        While reader.Read()
+                            ' Supongamos que los datos están en columnas "Nombre" y "Apellido"
+                            data &= "Nombre: " & reader("Nombre").ToString() & ", Email: " & reader("Email").ToString() & vbCrLf
+                        End While
+                    End Using
+                End Using
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error al obtener datos de la base de datos: " & ex.Message)
+        End Try
+
+        Return data
+    End Function
 
 
 
