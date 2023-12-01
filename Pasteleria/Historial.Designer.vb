92af30a5e4d55d1fded0caeb98d8f711c97cb366
Fabian1 <60055343+fabianlanza@users.noreply.github.com>
2023-11-30 20:35:11 -0600

    iText PDF

--- Pasteleria/Historial.Designer.vb
+++ Pasteleria/Historial.Designer.vb
@@ -23,25 +23,38 @@ Partial Class Historial
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.dgvHistorial = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.dgvHistorial, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'dgvHistorial
         '
         Me.dgvHistorial.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvHistorial.Location = New System.Drawing.Point(12, 235)
+        Me.dgvHistorial.Location = New System.Drawing.Point(7, 108)
+        Me.dgvHistorial.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.dgvHistorial.Name = "dgvHistorial"
         Me.dgvHistorial.RowHeadersWidth = 51
         Me.dgvHistorial.RowTemplate.Height = 24
-        Me.dgvHistorial.Size = New System.Drawing.Size(809, 328)
+        Me.dgvHistorial.Size = New System.Drawing.Size(607, 266)
         Me.dgvHistorial.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(249, 413)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 41)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Crear PDF"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Historial
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(833, 619)
+        Me.ClientSize = New System.Drawing.Size(625, 503)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.dgvHistorial)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Historial"
         Me.Text = "Historial"
         CType(Me.dgvHistorial, System.ComponentModel.ISupportInitialize).EndInit()
@@ -50,4 +63,5 @@ Partial Class Historial
     End Sub
 
     Friend WithEvents dgvHistorial As DataGridView
+    Friend WithEvents Button1 As Button
 End Class
--- Pasteleria/Historial.vb
+++ Pasteleria/Historial.vb
@@ -1,7 +1,45 @@
-﻿Public Class Historial
+﻿Imports iText.Layout
+Imports iText.Kernel.Pdf
+Imports iText.Layout.Element
+
+Public Class Historial
     Private Sub Historial_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LoadData(dgvHistorial, "Pastel")
     End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Specify the folder path where you want to save the PDF
+        Dim folderPath As String = "E:\Github\Reposteria\Pasteleria\pdf\" ' Update this with your desired folder path
+
+        ' Specify the file name for the PDF
+        Dim fileName As String = "output.pdf"
+
+        ' Combine the folder path and file name to create the full file path
+        Dim filePath As String = System.IO.Path.Combine(folderPath, fileName)
+
+        ' Create a new PDF document
+        Using pdfDoc As New PdfDocument(New PdfWriter(filePath))
+            ' Create a document
+            Dim document As New Document(pdfDoc)
+
+            ' Add content to the PDF
+            document.Add(New Paragraph("Hello, iText 7 in VB.NET!"))
+
+            ' Close the document
+            document.Close()
+        End Using
+
+        ' Open the generated PDF file using the default viewer
+        Process.Start(filePath)
+    End Sub
+
+
+
+
+
+
 
 
 
