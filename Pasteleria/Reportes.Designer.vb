043cbd008f9bde9395b4b0c10c921ecaaeb9248e
Fabian1 <60055343+fabianlanza@users.noreply.github.com>
2023-12-02 21:04:58 -0600

    Reportes Actualizacion

--- Pasteleria/Reportes.Designer.vb
+++ Pasteleria/Reportes.Designer.vb
@@ -23,29 +23,40 @@ Partial Class Reportes
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.cmbReporte = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(323, 236)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Location = New System.Drawing.Point(242, 192)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(160, 50)
+        Me.Button1.Size = New System.Drawing.Size(120, 41)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Crear PDF"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'cmbReporte
+        '
+        Me.cmbReporte.FormattingEnabled = True
+        Me.cmbReporte.Items.AddRange(New Object() {"Mostrar Clientes", "Mostrar Pasteles"})
+        Me.cmbReporte.Location = New System.Drawing.Point(242, 139)
+        Me.cmbReporte.Name = "cmbReporte"
+        Me.cmbReporte.Size = New System.Drawing.Size(121, 21)
+        Me.cmbReporte.TabIndex = 2
+        '
         'Reportes
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(833, 619)
+        Me.ClientSize = New System.Drawing.Size(625, 503)
+        Me.Controls.Add(Me.cmbReporte)
         Me.Controls.Add(Me.Button1)
-        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Reportes"
         Me.Text = "Reportes"
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents Button1 As Button
+    Friend WithEvents cmbReporte As ComboBox
 End Class
--- Pasteleria/Reportes.vb
+++ Pasteleria/Reportes.vb
@@ -4,6 +4,7 @@ Imports iText.Layout.Element
 Imports System.IO
 Imports System.Data.SqlClient
 Imports iText.IO.Image
+Imports iText.Layout.Properties
 
 Public Class Reportes
     Private Sub Reportes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -13,33 +14,43 @@ Public Class Reportes
 
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sentencia As String = ""
+        Dim Header As String = ""
+
+
+        ' Aquí establecemos la consulta SQL según la opción seleccionada en el ComboBox
+        If cmbReporte.SelectedItem.ToString() = "Mostrar Clientes" Then
+            sentencia = "SELECT * FROM Cliente"
+            Header = "Mostrar Clientes" 'Cambiar titulo y sentencias por las que usaremos
+        ElseIf cmbReporte.SelectedItem.ToString() = "Mostrar Pasteles" Then
+            sentencia = "SELECT Tipo, Cantidad FROM Pastel WHERE Cantidad > 0;"
+            Header = "Mostrar Pasteles"
+        End If
+
         Try
             ' Obtener la ruta de la imagen relativa al directorio actual
             Dim imagePath As String = Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName, "Img", "Logo.jpg")
 
-
             ' Mostrar la ruta de la imagen para verificar
-            MessageBox.Show("Ruta de la imagen: " & imagePath)
-
-            ' Verificar el directorio de trabajo actual
-            MessageBox.Show("Directorio de trabajo: " & Application.StartupPath)
+            'MessageBox.Show("Ruta de la imagen: " & imagePath)
+            'MessageBox.Show("Directorio de trabajo: " & Application.StartupPath)
 
             ' Query SQL para seleccionar todos los datos de la tabla "Clientes"
-            Dim query As String = "SELECT * FROM Cliente" ' Cambiar aquí por la tabla y columnas deseadas
+            Dim query As String = sentencia ' Cambiar aquí por la tabla y columnas deseadas
 
-            ' Obtener los datos de la base de datos utilizando la función GetDataFromDatabase
-            Dim data As String = GetDataFromDatabase(query)
+            ' Obtener los datos de la base de datos utilizando la función GetDataFromDatabase con la consulta específica
+            Dim data As String = GetDataFromDatabase(sentencia)
 
             ' Obtener la ruta de la carpeta Descargas
             Dim downloadsPath As String = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
             downloadsPath = Path.Combine(downloadsPath, "Downloads")
 
             ' Ruta del archivo PDF
-            Dim filePath As String = Path.Combine(downloadsPath, "clientes.pdf")
+            Dim filePath As String = Path.Combine(downloadsPath, "Reporte.pdf")
 
             ' Crear un nuevo documento PDF en la carpeta Descargas
             Using pdfDoc As New PdfDocument(New PdfWriter(filePath))
-                ' Crear un documento
+
                 Dim document As New Document(pdfDoc)
 
                 ' Cargar la imagen desde la ruta especificada
@@ -48,9 +59,27 @@ Public Class Reportes
                 ' Ajustar tamaño de la imagen si es necesario
                 image.SetWidth(100) ' Ajustar el ancho de la imagen según tus necesidades
                 image.SetHeight(100) ' Ajustar la altura de la imagen según tus necesidades
-
+                image.SetHorizontalAlignment(HorizontalAlignment.CENTER)
                 ' Agregar la imagen al PDF
                 document.Add(image)
+
+                'titulo
+                Dim titulo As New Paragraph(Header)
+                titulo.SetFontSize(24)
+                titulo.SetTextAlignment(TextAlignment.CENTER)
+                document.Add(titulo)
+
+
+                'Fecha
+                Dim fechaActual As String = DateTime.Now.ToString("dd/MM/yyyy")
+                Dim fechaTexto As New Paragraph("Fecha: " & fechaActual)
+                fechaTexto.SetFontSize(12) ' Tamaño del texto de la fecha
+                fechaTexto.SetTextAlignment(TextAlignment.RIGHT)
+                ' Agregamos la fecha al documento
+                document.Add(fechaTexto)
+
+
+
 
                 ' Agregar los datos obtenidos de la base de datos al PDF como un párrafo
                 document.Add(New Paragraph(data))
@@ -67,10 +96,6 @@ Public Class Reportes
         End Try
     End Sub
 
-
-
-
-
     Private Function GetDataFromDatabase(query As String) As String
         Dim data As String = ""
 
@@ -86,8 +111,14 @@ Public Class Reportes
                     Using reader As SqlDataReader = command.ExecuteReader()
                         ' Lee los datos y almacenarlos en la cadena 'data'
                         While reader.Read()
-                            ' Supongamos que los datos están en columnas "Nombre" y "Apellido"
-                            data &= "Nombre: " & reader("Nombre").ToString() & ", Email: " & reader("Email").ToString() & vbCrLf
+                            ' Verifica la consulta y construye la cadena 'data' según los campos específicos
+                            If query.Contains("Cliente") Then
+                                ' Si la consulta es para clientes, muestra los datos de Nombre y Email
+                                data &= "Nombre De Cliente: " & reader("Nombre").ToString() & ", Email: " & reader("Email").ToString() & vbCrLf
+                            ElseIf query.Contains("Pastel") Then
+                                ' Si la consulta es para pasteles, muestra los datos de Tipo y Cantidad
+                                data &= "Tipo: " & reader("Tipo").ToString() & ", Cantidad: " & reader("Cantidad").ToString() & vbCrLf
+                            End If
                         End While
                     End Using
                 End Using
